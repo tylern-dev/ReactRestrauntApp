dd333439b06dd257b4ff056ae8a0b9abac19ef2c
Tyler Negro <tnegro@gmail.com>
2018-04-10 17:22:19 -0600

    fish details

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,46 @@
 import React from "react";
-import Header from "./Header"
-import Order from "./Order"
-import Inventory from "./Inventory"
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes"
+import Fish from './Fish';
+
 class App extends React.Component {
-	state = {
-		fishes: {},
-		order: {}
-	}
+  state = {
+    fishes: {},
+    order: {}
+  };
 
-	addFish = (fish) => {
-		// 1. Take a copy of the existing state - never mutate state directly
-		const fishes = {...this.state.fishes}; //object spread
-		// 2. Add new fish to that fishes variable
-		fishes[`fish${Date.now()}`] = fish;
-		// 3. Place the fishes object into state
-		this.setState({
-			fishes: fishes
-		})
-	}
+  addFish = fish => {
+    // 1. Take a copy of the existing state - never mutate state directly
+    const fishes = { ...this.state.fishes }; //object spread
+    // 2. Add new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Place the fishes object into state
+    this.setState({
+      fishes: fishes
+    });
+  };
 
-	render() {
-		return (
-			<div className="catch-of-the-day">
-				<div className="menu">
-					<Header tagline="fresh seafood market" />
-				</div>
-				<Order />
-				<Inventory addFish={this.addFish} />
-			</div>
-		)
-	}
+  loadSampleFishes = (event) => {
+    event.preventDefault();
+    this.setState({ fishes: sampleFishes })
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="fresh seafood market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+          </ul>
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -9,6 +9,7 @@ export default class Inventory extends Component {
 
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
       </div>
     )
   }
